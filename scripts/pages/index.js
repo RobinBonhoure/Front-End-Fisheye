5d50e87e8e827d7f11dd1f1fc539338a682c0b2c
robin-mk <robin.meltingk@gmail.com>
2021-12-02 18:47:11 +0100

    get json in javascript and show photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,37 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
-
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
+    var request = new XMLHttpRequest();
+    request.open("GET", "data/photographers.json", false);
+    request.send(null);
+    var my_JSON_object = JSON.parse(request.responseText);
+    // request.onreadystatechange = function () {
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         var my_JSON_object = JSON.parse(request.responseText);
+    //         console.log(my_JSON_object);
+    //     }
+    // }
+    const photographers = [my_JSON_object.photographers];
     
-    init();
-    
+    console.log(photographers)
+    return ({
+        photographers: [...photographers[0]]
+    })
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
